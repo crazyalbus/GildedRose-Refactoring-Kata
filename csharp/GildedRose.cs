960f864aba543a048059c7e4823071930de00dad
Clare Sudbery <clareworking@claresudbery.co.uk>
2022-04-28 19:51:53 +0100

    Replaced Aged Brie magic strings with constants

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -14,7 +14,7 @@ namespace csharp
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (Items[i].Name != ItemNames.AgedBrie && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                 {
                     if (Items[i].Quality > 0)
                     {
@@ -58,7 +58,7 @@ namespace csharp
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Items[i].Name != ItemNames.AgedBrie)
                     {
                         if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                         {
---
+++ csharp/ItemNames.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp
+{
+    public static class ItemNames
+    {
+        public const string AgedBrie = "Aged Brie";
+        public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        public const string BackstagePasses = "Backstage passes";
+        public const string Conjured = "Conjured Mana Cake";
+    }
+}

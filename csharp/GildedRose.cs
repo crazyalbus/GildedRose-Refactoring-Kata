c07324674d1f053c06d95adc0eaf526cda55d436
Clare Sudbery <clareworking@claresudbery.co.uk>
2022-09-19 19:57:03 +0100

    extract item names to separate class

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -2,14 +2,6 @@
 
 namespace csharp
 {
-    public static class ItemNames
-    {
-        public const string AgedBrie = "Aged Brie";
-        public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
-        public const string BackstagePasses = "Backstage passes";
-        public const string Conjured = "Conjured Mana Cake";
-    }
-
     public class GildedRose
     {
         IList<Item> Items;

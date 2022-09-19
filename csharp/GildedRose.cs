e92146a0f7f7703ae15c6c96642206831c4cc926
Clare Sudbery <clareworking@claresudbery.co.uk>
2022-09-19 19:56:06 +0100

    code after first live demo

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -2,9 +2,19 @@
 
 namespace csharp
 {
+    public static class ItemNames
+    {
+        public const string AgedBrie = "Aged Brie";
+        public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+        public const string BackstagePasses = "Backstage passes";
+        public const string Conjured = "Conjured Mana Cake";
+    }
+
     public class GildedRose
     {
         IList<Item> Items;
+        private const int MaxQuality = 50;
+
         public GildedRose(IList<Item> Items)
         {
             this.Items = Items;
@@ -14,11 +24,11 @@ namespace csharp
         {
             for (var i = 0; i < Items.Count; i++)
             {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (Item_quality_decreases_with_age(i))
                 {
                     if (Items[i].Quality > 0)
                     {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                        if (Items[i].Name != ItemNames.Sulfuras)
                         {
                             Items[i].Quality = Items[i].Quality - 1;
                         }
@@ -26,15 +36,15 @@ namespace csharp
                 }
                 else
                 {
-                    if (Items[i].Quality < 50)
+                    if (Items[i].Quality < MaxQuality)
                     {
-                        Items[i].Quality = Items[i].Quality + 1;
+                        Increment_quality(i);
 
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (Item_is_backstage_pass(Items[i].Name))
                         {
                             if (Items[i].SellIn < 11)
                             {
-                                if (Items[i].Quality < 50)
+                                if (Items[i].Quality < MaxQuality)
                                 {
                                     Items[i].Quality = Items[i].Quality + 1;
                                 }
@@ -42,7 +52,7 @@ namespace csharp
 
                             if (Items[i].SellIn < 6)
                             {
-                                if (Items[i].Quality < 50)
+                                if (Items[i].Quality < MaxQuality)
                                 {
                                     Items[i].Quality = Items[i].Quality + 1;
                                 }
@@ -51,20 +61,20 @@ namespace csharp
                     }
                 }
 
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                if (Items[i].Name != ItemNames.Sulfuras)
                 {
                     Items[i].SellIn = Items[i].SellIn - 1;
                 }
 
                 if (Items[i].SellIn < 0)
                 {
-                    if (Items[i].Name != "Aged Brie")
+                    if (Items[i].Name != ItemNames.AgedBrie)
                     {
                         if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
                         {
                             if (Items[i].Quality > 0)
                             {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
+                                if (Items[i].Name != ItemNames.Sulfuras)
                                 {
                                     Items[i].Quality = Items[i].Quality - 1;
                                 }
@@ -77,13 +87,29 @@ namespace csharp
                     }
                     else
                     {
-                        if (Items[i].Quality < 50)
+                        if (Items[i].Quality < MaxQuality)
                         {
                             Items[i].Quality = Items[i].Quality + 1;
                         }
                     }
                 }
             }
+        }
+
+        private void Increment_quality(int item_index)
+        {
+            Items[item_index].Quality = Items[item_index].Quality + 1;
+        }
+
+        private bool Item_is_backstage_pass(string name)
+        {
+            return name == "Backstage passes to a TAFKAL80ETC concert";
+        }
+
+        private bool Item_quality_decreases_with_age(int item_index)
+        {
+            return (Items[item_index].Name != ItemNames.AgedBrie &&
+                    Items[item_index].Name != "Backstage passes to a TAFKAL80ETC concert");
         }
     }
 }

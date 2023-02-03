03e78f9e8eb502d118312b787a63a466b4b6acfd
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-02-03 17:02:39 +0000

    Remove redundant braces

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -23,27 +23,25 @@ namespace csharp
             var isAgedBrie = item.Name == "Aged Brie";
             if (isAgedBrie)
             {
+                if (item.Quality < 50)
                 {
-                    if (item.Quality < 50)
+                    item.Quality = item.Quality + 1;
+
+                    if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
                     {
-                        item.Quality = item.Quality + 1;
-
-                        if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+                        if (item.SellIn < 11)
                         {
-                            if (item.SellIn < 11)
+                            if (item.Quality < 50)
                             {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
+                                item.Quality = item.Quality + 1;
                             }
+                        }
 
-                            if (item.SellIn < 6)
+                        if (item.SellIn < 6)
+                        {
+                            if (item.Quality < 50)
                             {
-                                if (item.Quality < 50)
-                                {
-                                    item.Quality = item.Quality + 1;
-                                }
+                                item.Quality = item.Quality + 1;
                             }
                         }
                     }

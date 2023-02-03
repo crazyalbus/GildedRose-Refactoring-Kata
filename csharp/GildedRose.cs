5472636128f5df344ca4e591de81914210c7106e
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-02-03 17:00:25 +0000

    Replace always-false expression with false

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -23,7 +23,7 @@ namespace csharp
             var isAgedBrie = item.Name == "Aged Brie";
             if (isAgedBrie)
             {
-                if (!isAgedBrie && item.Name != "Backstage passes to a TAFKAL80ETC concert")
+                if (false)
                 {
                     if (item.Quality > 0)
                     {

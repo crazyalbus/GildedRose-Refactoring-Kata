cfdff96928d9445e832f87bfd22b673a2d5050a0
Clare Sudbery <clareworking@claresudbery.co.uk>
2023-02-22 00:07:36 +0000

    Added new equivalent expressions and if statements

---
+++ csharp/BooleanParsingTests.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using ApprovalTests.Combinations;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+
+namespace csharp
+{
+    [UseReporter(typeof(DiffReporter))]
+    [TestFixture]
+    public class BooleanParsingTests
+    {
+        [Test]
+        public void BooleanParsingEx1()
+        {
+            CombinationApprovals.VerifyAllCombinations(
+                DoBooleanParsingEx1,
+                new String[] { "green", "blue" },
+                new String[] { "green", "blue" });
+        }
+
+        private String DoBooleanParsingEx1(String colour1, String colour2)
+        {
+            bool result1 = (colour1 == colour1 && colour2 == colour2);
+            bool result2 = true;
+            return $"{result1 == result2}";
+        }
+
+        [Test]
+        public void BooleanParsingEx2()
+        {
+            CombinationApprovals.VerifyAllCombinations(
+                DoBooleanParsingEx2,
+                new String[] { "green", "blue" },
+                new String[] { "green", "blue" });
+        }
+
+        private String DoBooleanParsingEx2(String colour1, String colour2)
+        {
+            bool result1 = (colour1 == "green" && colour1 == "blue");
+            bool result2 = false;
+            return $"{result1 == result2}";
+        }
+
+        [Test]
+        public void BooleanParsingEx3()
+        {
+            CombinationApprovals.VerifyAllCombinations(
+                DoBooleanParsingEx3,
+                new String[] { "green", "blue" },
+                new String[] { "green", "blue" });
+        }
+
+        private String DoBooleanParsingEx3(String colour1, String colour2)
+        {
+            bool result1 = (colour1 != "green" && colour2 != "blue");
+            bool result2 = !(colour1 == "green" || colour2 == "blue");
+            return $"{result1 == result2}";
+        }
+
+        [Test]
+        public void BooleanParsingEx4()
+        {
+            CombinationApprovals.VerifyAllCombinations(
+                DoBooleanParsingEx4,
+                new String[] { "green", "blue" },
+                new String[] { "green", "blue" });
+        }
+
+        private String DoBooleanParsingEx4(String colour1, String colour2)
+        {
+            bool result1 = !(colour1 != "green" && colour2 != "blue");
+            bool result2 = (colour1 == "green" || colour2 == "blue");
+            return $"{result1 == result2}";
+        }
+
+        [Test]
+        public void BooleanParsingEx5()
+        {
+            CombinationApprovals.VerifyAllCombinations(
+                DoBooleanParsingEx5,
+                new String[] { "green", "blue" },
+                new String[] { "green", "blue" });
+        }
+
+        private String DoBooleanParsingEx5(String colour1, String colour2)
+        {
+            bool result1 = !(colour1 == "green" && colour2 == "blue");
+            bool result2 = (colour1 != "green") || (colour2 != "blue");
+            return $"{result1 == result2}";
+        }
+
+        [Test]
+        public void BooleanParsingEx6()
+        {
+            CombinationApprovals.VerifyAllCombinations(
+                DoBooleanParsingEx6,
+                new String[] { "green", "blue" },
+                new String[] { "green", "blue" });
+        }
+
+        private String DoBooleanParsingEx6(String colour1, String colour2)
+        {
+            bool result1 = !(colour1 == "green" || colour2 == "blue");
+            bool result2 = (colour1 != "green") && (colour2 != "blue");
+            return $"{result1 == result2}";
+        }
+
+        [Test]
+        public void BooleanParsingTestOtherGuesses()
+        {
+            CombinationApprovals.VerifyAllCombinations(
+                DoBooleanParsingTestOtherGuesses,
+                new String[] { "green", "blue" },
+                new String[] { "green", "blue" });
+        }
+
+        private String DoBooleanParsingTestOtherGuesses(String colour1, String colour2)
+        {
+            bool result1 = true;
+            bool result2 = true;
+            return $"{result1 == result2}";
+        }
+    }
+}
---
+++ csharp/IfParsingTests.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using ApprovalTests.Combinations;
+using ApprovalTests.Reporters;
+using NUnit.Framework;
+
+namespace csharp
+{
+    [UseReporter(typeof(DiffReporter))]
+    [TestFixture]
+    public class IfParsingTests
+    {
+        [Test]
+        public void IfParsingEx1()
+        {
+            CombinationApprovals.VerifyAllCombinations(
+                DoIfParsingEx1,
+                new String[] { "green", "blue" },
+                new String[] { "green", "blue" });
+        }
+
+        private String DoIfParsingEx1(String colour1, String colour2)
+        {
+            bool result1 = false;
+            bool result2 = false;
+            if((colour1 == colour1 && colour2 == colour2))
+            {
+                result1 = true;
+            }
+            if(true)
+            {
+                result2 = true;
+            }
+            return $"{result1 && result2}";
+        }
+
+        [Test]
+        public void IfParsingEx2()
+        {
+            CombinationApprovals.VerifyAllCombinations(
+                DoIfParsingEx2,
+                new String[] { "green", "blue" },
+                new String[] { "green", "blue" });
+        }
+
+        private String DoIfParsingEx2(String colour1, String colour2)
+        {
+            bool result1 = false;
+            bool result2 = false;
+            if((colour1 == "green" && colour1 == "blue"))
+            {
+                result1 = true;
+            }
+            if(false)
+            {
+                result2 = true;
+            }
+            return $"{result1 && result2}";
+        }
+
+        [Test]
+        public void IfParsingEx3()
+        {
+            CombinationApprovals.VerifyAllCombinations(
+                DoIfParsingEx3,
+                new String[] { "green", "blue" },
+                new String[] { "green", "blue" });
+        }
+
+        private String DoIfParsingEx3(String colour1, String colour2)
+        {
+            bool result1 = false;
+            bool result2 = false;
+            if((colour1 != "green" && colour2 != "blue"))
+            {
+                result1 = true;
+            }
+            if(!(colour1 == "green" || colour2 == "blue"))
+            {
+                result2 = true;
+            }
+            return $"{result1 && result2}";
+        }
+
+        [Test]
+        public void IfParsingEx4()
+        {
+            CombinationApprovals.VerifyAllCombinations(
+                DoIfParsingEx4,
+                new String[] { "green", "blue" },
+                new String[] { "green", "blue" });
+        }
+
+        private String DoIfParsingEx4(String colour1, String colour2)
+        {
+            bool result1 = false;
+            bool result2 = false;
+            if(!(colour1 != "green" && colour2 != "blue"))
+            {
+                result1 = true;
+            }
+            if((colour1 == "green" || colour2 == "blue"))
+            {
+                result2 = true;
+            }
+            return $"{result1 && result2}";
+        }
+
+        [Test]
+        public void IfParsingEx5()
+        {
+            CombinationApprovals.VerifyAllCombinations(
+                DoIfParsingEx5,
+                new String[] { "green", "blue" },
+                new String[] { "green", "blue" });
+        }
+
+        private String DoIfParsingEx5(String colour1, String colour2)
+        {
+            bool result1 = false;
+            bool result2 = false;
+            if(!(colour1 == "green" && colour2 == "blue"))
+            {
+                result1 = true;
+            }
+            if((colour1 != "green") || (colour2 != "blue"))
+            {
+                result2 = true;
+            }
+            return $"{result1 && result2}";
+        }
+
+        [Test]
+        public void IfParsingEx6()
+        {
+            CombinationApprovals.VerifyAllCombinations(
+                DoIfParsingEx6,
+                new String[] { "green", "blue" },
+                new String[] { "green", "blue" });
+        }
+
+        private String DoIfParsingEx6(String colour1, String colour2)
+        {
+            bool result1 = false;
+            bool result2 = false;
+            if(!(colour1 == "green" || colour2 == "blue"))
+            {
+                result1 = true;
+            }
+            if((colour1 != "green") && (colour2 != "blue"))
+            {
+                result2 = true;
+            }
+            return $"{result1 && result2}";
+        }
+
+        [Test]
+        public void IfParsingTestOtherGuesses()
+        {
+            CombinationApprovals.VerifyAllCombinations(
+                DoIfParsingTestOtherGuesses,
+                new String[] { "green", "blue" },
+                new String[] { "green", "blue" });
+        }
+
+        private String DoIfParsingTestOtherGuesses(String colour1, String colour2)
+        {
+            bool result1 = false;
+            bool result2 = false;
+            if(true)
+            {
+                result1 = true;
+            }
+            if(true)
+            {
+                result2 = true;
+            }
+            return $"{result1 && result2}";
+        }
+    }
+}
